1473dcd4ac1a812c89cf22fb9c185a8ce8cb5ecf
imsparkk <nikitarv2008@gmail.com>
2026-03-05 18:12:33 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:pneuma_messenger/pages/register_page.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(home: RegisterPage()));
