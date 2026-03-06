bec9cdc2315805cae4f4d619b8388331409bcde0
imsparkk <nikitarv2008@gmail.com>
2026-03-07 03:56:26 +0500

    register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pneuma_messenger/pages/register_page.dart';
 
-void main() => runApp(MaterialApp(home: RegisterPage()));
+void main() => runApp(
+  MaterialApp(
+    home: RegisterPage()
+    )
+);
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,13 +1,193 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+   bool passwordVisible=false;
+    
+   @override
+    void initState(){
+      super.initState();
+      passwordVisible=true;
+    }   
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("text"),
+      backgroundColor: Color(0x00022866),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pneuma",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50,
+              ),
+              ),
+
+              SizedBox(height: 15,),
+
+              Center(
+                child: SizedBox(
+                  width: 400,
+                  child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(
+                      Icons.person, 
+                      color: Colors.black,
+                      size: 30,
+                      ),
+                    prefixIconConstraints: BoxConstraints(minWidth: 60),
+                    hintText: "Login",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(style: BorderStyle.none)
+                    ),
+                  
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(width: 1, color: Colors.red, style: BorderStyle.solid)
+                    )
+                  ),  
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15,),
+
+              Center(
+                child: SizedBox(
+                  width: 400,
+                  child: TextField(
+                    maxLength: 25,
+                  obscureText: passwordVisible,
+                  decoration: InputDecoration(
+                    counterText: "",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(
+                      Icons.lock, 
+                      color: Colors.black,
+                      size: 30,
+                      ),
+                    prefixIconConstraints: BoxConstraints(minWidth: 60),
+                    hintText: "Password",
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.only(right: 10),
+                      iconSize: 30,
+                      icon: Icon(
+                        color: Colors.black,
+                        passwordVisible ? Icons.visibility
+                        : Icons.visibility_off,),
+                        onPressed: () {
+                          setState(() {
+                            passwordVisible = !passwordVisible;
+                          });
+                        },
+                        
+                      ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(style: BorderStyle.none)
+                    ),
+                  
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(width: 1, color: Colors.red, style: BorderStyle.solid)
+                    )
+                  ),  
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15,),
+
+              Center(
+                child: SizedBox(
+                  width: 400,
+                  child: TextField(
+                    maxLength: 25,
+                    
+                  obscureText: passwordVisible,
+                  decoration: InputDecoration(
+                    counterText: "",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(
+                      Icons.lock, 
+                      color: Colors.black,
+                      size: 30,
+                      ),
+                    prefixIconConstraints: BoxConstraints(minWidth: 60),
+                    hintText: "Password",
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.only(right: 10),
+                      iconSize: 30,
+                      icon: Icon(
+                        color: Colors.black,
+                        passwordVisible ? Icons.visibility
+                        : Icons.visibility_off,),
+                        onPressed: () {
+                          setState(() {
+                            passwordVisible = !passwordVisible;
+                          });
+                        },
+                        
+                      ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(style: BorderStyle.none)
+                    ),
+                  
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderSide: BorderSide(width: 1, color: Colors.red, style: BorderStyle.solid)
+                    )
+                  ),  
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15,),
+
+              ElevatedButton(
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                  backgroundColor: Colors.white,
+                  color: Colors.black
+                  ),
+                ),
+                onPressed: () {},
+                ),
+                TextButton(
+                  child: Text(
+                    "Есть учетная запись? Авторизуйтесь!",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                  )
+          ],
       ),
     );
   }
